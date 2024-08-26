7bbd9082bc8e7462eeb98aebbaf4caf3b2cc6cf0
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-08-25 20:20:05 -0600

    removed python

--- setup.sh
+++ setup.sh
@@ -1,11 +1,2 @@
 # Update package lists
-apt update -y
-
-# Upgrade installed packages
-apt upgrade -y
-
-# Install pip required for Python packages
-apt install python3-pip -y
-
-# Install Python packages
-pip3 install -r requirements.txt
+apt update -y & apt upgrade -y & pip3 install -r requirements.txt
