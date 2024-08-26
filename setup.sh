153d1daa9d5be8ffe080ce030601902c25505343
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-08-25 19:53:53 -0600

    pip install requirements

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@ sudo apt update -y
 
 # Upgrade installed packages
 sudo apt upgrade -y
+
+# Install pip required for Python packages
+sudo apt install python3-pip -y
+
+# Install Python packages
+pip3 install -r requirements.txt
