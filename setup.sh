52fdd4bfa71b38b37d0f0e16bc42511efc1be175
Guillermo Alcántara <32722692+Echo9k@users.noreply.github.com>
2024-08-25 20:12:43 -0600

    removed sudo commands

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 # Update package lists
-sudo apt update -y
+apt update -y
 
 # Upgrade installed packages
-sudo apt upgrade -y
+apt upgrade -y
 
 # Install pip required for Python packages
-sudo apt install python3-pip -y
+apt install python3-pip -y
 
 # Install Python packages
 pip3 install -r requirements.txt
